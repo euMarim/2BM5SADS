82b643755a00c50584c6d7d205188f0a481ce2d4
euMarim <lucas_mf2000@hotmail.com>
2025-05-08 20:03:51 -0300

    project

---
+++ src/main/java/com/exemplo/JWTproject/JwTprojectApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.JWTproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwTprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwTprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exemplo/JWTproject/JwTprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemplo.JWTproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwTprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
